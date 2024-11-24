88c53083e4dad8bd1f426867aff9f5453a06364b
Umma Ahimsha <uahimsha@gmail.com>
2024-11-25 01:22:35 +0700

    init billiard amin app

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,18 +1,18 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path"
+import { bytecodePlugin, defineConfig, externalizeDepsPlugin } from "electron-vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()],
   },
   renderer: {
     resolve: {
       alias: {
-        '@': resolve('src/renderer/src'),
+        "@": resolve("src/renderer/src"),
       },
     },
     plugins: [react()],
